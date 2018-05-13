e976affca67180e2290a65edaef9e00dc7eacab8
知 未 <i237731947@outlook.com>
2018-05-13 16:00:45 +0800

    添加Admin类

--- src/twt/model/Admin.java
+++ src/twt/model/Admin.java
@@ -1,5 +1,31 @@
 package twt.model;
 
 public class Admin {
-
+	public int id;
+	public String name;
+	public String phone;
+	public int getId() {
+		return id;
+	}
+	public Admin(int id, String name, String phone) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.phone = phone;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
 }
